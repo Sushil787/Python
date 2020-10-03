1a2d833783e7002d6a905b26f38439b4f522711d
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-03 19:57:40 +0545

    update
    I just created multiple file using list and allowed to write user in the file.

--- filehand.py
+++ filehand.py
@@ -1,3 +1,7 @@
-file=open("filehand.txt","r")
-x=file.read()
-print(x)
+items = ["one","two","three"]
+
+for item in items:
+    with open("{}hello_world.txt".format(item), "w") as f:
+        string = input(f"enter any thing you like to write in {one} file: ")
+        f.write(string)
+        f.write("\nThis is my second line of code with {} the first item in my list".format(item))
